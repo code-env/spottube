dce8b2f47db18547e5a63413822b0415ec024644
code-env <org.codeen@gmail.com>
2024-10-22 14:54:08 +0100

    feat: project initialized

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: 'Spottube',
+  description: 'transfer your youtube playlist to spotify',
+  url: 'https://spottube.bossadizenith.me',
+  og: 'https://spottube.bossadizenith.me/og.png',
+  links: {
+    twitter: '@bossadizenith',
+    github: 'https://github.com/code-env',
+    linkedin: 'https://www.linkedin.com/in/codeenv/',
+  },
+};
